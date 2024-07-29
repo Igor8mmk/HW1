a463c9cf5d4306d1d43d317a5b43b445e42765e4
Igor8mmk <igor.mmk1@gmail.com>
2024-07-29 19:17:57 +0300

    Add files via upload

---
+++ Homework1.py
@@ -0,0 +1,10 @@
+# 1
+print ((9 ** 0.5)*5)
+# 2st program
+print(9.99>9.98 and 1000!=1001)
+# 3rd program
+Number1 = ((1234%1000)//10)
+Number2 = ((5678%1000)//10)
+print(Number1+Number2)
+# 4rd program
+print (int(42.13)==int(13.42*100)%100)
